bbf11e949e4eee243f72791d71601e639e597fa6
saktiworkstation <saktiworkstation@gmail.com>
2023-12-15 17:48:31 +0700

    Added layouts main, partials vue, and homepage. Also a little content

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+        <a class="navbar-brand" href="#">Winter Wonder</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+            aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Tickets</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+            </ul>
+            <span class="navbar-text">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item px-3">
+                        <a class="btn btn-outline-info" href="#">Sign In</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn btn-info" href="#">Sign Up</a>
+                    </li>
+                </ul>
+            </span>
+        </div>
+    </div>
+</nav>
