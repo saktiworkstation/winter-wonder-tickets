df55f4eb429b9e8ebeb8e89afa9eb17e512f4d87
saktiworkstation <saktiworkstation@gmail.com>
2023-12-16 23:47:24 +0700

    Added content to the homepage and several improvements

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,37 +1,94 @@
 @extends('layouts.main')
 
 @section('container')
-    <div class="row d-flex align-content-end">
-        <div class="col-md-7 mx-auto">
-            <h1 class="d-flex align-self-center text-center">Say goodbye to the hassle of manual ticket booking, let our
-                system handle it
-                for
-                you!</h1>
-            <h4 class="d-flex align-self-center text-center">Experience the convenience of reserving tickets effortlessly for
-                your
-                favorite
-                attractions, whether it's
-                physical tickets or e-tickets. Your gateway to seamless adventures begins here!</h4>
+    <div class="container my-3">
+        <div class="row d-flex align-content-end">
+            <div class="col-md-7 mx-auto">
+                <h1 class="d-flex align-self-center text-center">Say goodbye to the hassle of manual ticket booking, let our
+                    system handle it
+                    for
+                    you!</h1>
+                <h4 class="d-flex align-self-center text-center">Experience the convenience of reserving tickets effortlessly
+                    for
+                    your
+                    favorite
+                    attractions, whether it's
+                    physical tickets or e-tickets. Your gateway to seamless adventures begins here!</h4>
+            </div>
         </div>
-    </div>
 
-    {{-- Ticket --}}
-    <div class="row pt-4">
-        <h2>Booking tickets now</h2>
-        <div class="col-md-4 py-3">
-            <div class="card" style="width: 18rem;">
-                <img src="/img/room.jpg" class="card-img-top" height="300">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's
-                        content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+        {{-- Ticket --}}
+        <div class="row pt-4 mt-3">
+            <h2 class="pb-3">Booking tickets now</h2>
+            <div class="col-md-4 py-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="/img/room.jpg" class="card-img-top" height="300">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's
+                            content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 py-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="/img/room.jpg" class="card-img-top" height="300">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's
+                            content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 py-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="/img/room.jpg" class="card-img-top" height="300">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's
+                            content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
     {{-- Promotion --}}
-    <div class="row"></div>
+    <div class="container-fluid bg-body-tertiary mt-3">
+        <div class="container py-3">
+            <div class="row">
+                <h2 class="pb-3">New Promotions</h2>
+                <div class="col-md-4 mx-1">
+                    <div class="card" style="width: 18rem;">
+                        <img src="/img/room.jpg" class="card-img-top" height="300" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of
+                                the
+                                card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mx-1">
+                    <div class="card" style="width: 18rem;">
+                        <img src="/img/room.jpg" class="card-img-top" height="300" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of
+                                the
+                                card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Winter Wonder Ticket</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
@@ -12,9 +12,56 @@
 <body>
     @include('partials.navbar')
 
-    <div class="container mt-3">
-        @yield('container')
-    </div>
+    @yield('container')
+
+    {{-- Footer --}}
+    <footer class="container-fluid border-top">
+        <div class="row">
+            <div class="col-12 col-md">
+                <img class="mb-2" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="24"
+                    height="19">
+                <small class="d-block mb-3 text-body-secondary">&copy; 2017–2023</small>
+            </div>
+            <div class="col-6 col-md">
+                <h5>Features</h5>
+                <ul class="list-unstyled text-small">
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Cool stuff</a></li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Random feature</a>
+                    </li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Team feature</a>
+                    </li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Stuff for
+                            developers</a></li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Another one</a>
+                    </li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Last time</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h5>Resources</h5>
+                <ul class="list-unstyled text-small">
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Resource</a></li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Resource name</a>
+                    </li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Another
+                            resource</a></li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Final resource</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h5>About</h5>
+                <ul class="list-unstyled text-small">
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Team</a></li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Locations</a></li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Privacy</a></li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Terms</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
