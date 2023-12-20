98e2cf7d4cad32e1648a8d4c0faa424c18f81f3d
saktiworkstation <saktiworkstation@gmail.com>
2023-12-20 22:27:17 +0700

    Add some content to promotion.index

--- resources/views/promotion/index.blade.php
+++ resources/views/promotion/index.blade.php
@@ -5,6 +5,15 @@
         <div class="row">
             <div class="col-md-8">
                 <h3>All Promotions</h3>
+                <form action="#" method="get">
+                    <div class="col-md-5">
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Search promo" aria-label="Search promo"
+                                aria-describedby="button-addon2">
+                            <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
+                        </div>
+                    </div>
+                </form>
                 <hr>
                 <div class="card mb-3" style="max-width: 540px;">
                     <div class="row g-0">
@@ -17,6 +26,7 @@
                                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
                                     additional content. This content is a little bit longer.</p>
                                 <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                                <a href="#" class="btn btn-primary">Claim Promo</a>
                             </div>
                         </div>
                     </div>
