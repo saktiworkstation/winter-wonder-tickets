003d43fcbad25091d4b96fc0737875d6c15239c2
saktiworkstation <saktiworkstation@gmail.com>
2023-12-28 20:28:34 +0700

    perfecting the main layouts, and creating the main dashboard page

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('/dashboard');
+            return redirect()->intended('/dashhboard');
         }
 
         return back()->with('loginError', 'Login failed!');
---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,5 @@
+@extends('dashboard.layots.main')
+
+@section('container')
+    {{--  --}}
+@endsection
