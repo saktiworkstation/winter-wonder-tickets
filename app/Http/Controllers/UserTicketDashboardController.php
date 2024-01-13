bd439dbec88656b08cb872e04ab3d28fd91463ed
saktiworkstation <saktiworkstation@gmail.com>
2024-01-13 19:50:22 +0700

    Create a User Ticket DashboardController and declare two functions

---
+++ app/Http/Controllers/UserTicketDashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserTicketDashboardController extends Controller
+{
+    public function UseTicket(){
+        // Fungsi untuk menggunakan ticket
+    }
+
+    public function UserTicketReport(){
+        // Fungsi Untuk halaman Report yang berisikan Data Penggunaan dan history pembelien ticket oleh user
+    }
+}
