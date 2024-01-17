28f3f833333f86ab09815d4f83a80393c4f3d66f
saktiworkstation <saktiworkstation@gmail.com>
2024-01-17 18:07:42 +0700

    Create a notification page for admin and members

--- app/Http/Controllers/NotificationDashboardController.php
+++ app/Http/Controllers/NotificationDashboardController.php
@@ -2,16 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Notification;
 use Illuminate\Http\Request;
 
 class NotificationDashboardController extends Controller
 {
-    public function adminAction(){
+    public function adminView(){
         // Fungsi untuk halaman admin, isi dari tampilan semua notif
+        return Response(view('dashboard.notification.admin',[
+            'notifications' => Notification::latest()->get()
+        ]));
     }
 
-    public function memberAction(){
+    public function memberView(){
         // Fungsi untuk halaman member, isi dari notif yang di dapatkan
+        return Response(view('dashboard.notification.member', [
+            'notifications' => Notification::where('user_id', auth()->user()->id)->latest()->get()
+        ]));
     }
 
     public function addNotifToAll(){
---
+++ resources/views/dashboard/notification/member.blade.php
@@ -0,0 +1,32 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">My Notifications</h1>
+    </div>
+
+    {{-- Ticket --}}
+    <div class="row pt-4 mt-3">
+        @if (session()->has('success'))
+            <div class="alert alert-success alert-dismissible fade show col-lg-8" role="alert">
+                {{ session('success') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+
+        @foreach ($notifications as $notif)
+            <div class="col-md-4 py-3">
+                <div class="card">
+                    <h5 class="card-header">{{ $notif->user->username }}</h5>
+                    <div class="card-body">
+                        <h5 class="card-title">Message : {{ $notif->message }}</h5>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+
+        <div class="d-flex justify-content-center">
+            {{ $userTickets->links() }}
+        </div>
+    </div>
+@endsection
