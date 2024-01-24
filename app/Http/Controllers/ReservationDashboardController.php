26b9f034a1737b4f160d7a9326a1b67eaf4e2604
saktiworkstation <saktiworkstation@gmail.com>
2024-01-24 21:33:22 +0700

    added ReservationController and some functions in it

---
+++ app/Http/Controllers/ReservationDashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReservationDashboardController extends Controller
+{
+    public function index(){
+        // halaman untuk menampilkan daftar lokasi untuk dilakukan reservasi
+    }
+
+    public function allReservations(){
+        // menampilkan semua data reservasi untuk di konfirmasi
+    }
+
+    public function memberReservations(){
+        // menampilkan data reservasi member
+    }
+}
