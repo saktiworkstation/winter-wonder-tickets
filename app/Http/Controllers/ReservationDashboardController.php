d594da697b6c45bef0b7a5b91593db04a1829eba
saktiworkstation <saktiworkstation@gmail.com>
2024-01-26 08:27:58 +0700

    Create create, do-reservation, index, management, member-reservation pages

--- app/Http/Controllers/ReservationDashboardController.php
+++ app/Http/Controllers/ReservationDashboardController.php
@@ -8,28 +8,33 @@ class ReservationDashboardController extends Controller
 {
     public function index(){
         // halaman untuk menampilkan daftar lokasi untuk dilakukan reservasi
+        return Response(view());
     }
 
-    public function allReservations(){
+    public function manageReservations(){
         // menampilkan semua data reservasi untuk di konfirmasi
+        return Response(view());
     }
 
     public function memberReservations(){
         // menampilkan data reservasi member
+        return Response(view());
     }
 
     public function addReservation(){
         // halaman menambahkan reservasi baru
+        return Response(view());
     }
 
-    public function makeReservation(){
+    public function doReservation(){
         // halaman untuk user melakukan reservasi
+        return Response(view());
     }
     public function storeAddReservation(){
         // menambahkan data reservasi baru
     }
 
-    public function storeMakeReservation(){
+    public function storeDoReservation(){
         // menambahkan data reservasi yang dilakukan user
     }
 }
