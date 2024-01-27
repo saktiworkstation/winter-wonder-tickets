c1bd1aeadd2e80d36895763895036c8d1469f6d2
saktiworkstation <saktiworkstation@gmail.com>
2024-01-27 10:48:47 +0700

    Added function to display reservation page for admin and members

--- app/Http/Controllers/ReservationDashboardController.php
+++ app/Http/Controllers/ReservationDashboardController.php
@@ -2,33 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reservation;
 use Illuminate\Http\Request;
 
 class ReservationDashboardController extends Controller
 {
     public function index(){
         // halaman untuk menampilkan daftar lokasi untuk dilakukan reservasi
-        return Response(view());
+        return Response(view('dashboard.reservation.index',[
+            'reservations' => Reservation::latest()->get(),
+        ]));
     }
 
     public function manageReservations(){
         // menampilkan semua data reservasi untuk di konfirmasi
-        return Response(view());
+        return Response(view('dashboard.reservation.management',[
+            'reservations' => Reservation::latest()->get(),
+        ]));
     }
 
     public function memberReservations(){
         // menampilkan data reservasi member
-        return Response(view());
+        return Response(view('dashboard.reservation.member-reservation', [
+            'reservations' => Reservation::where('user_id', auth()->user()->id)->get(),
+        ]));
     }
 
     public function addReservation(){
         // halaman menambahkan reservasi baru
-        return Response(view());
+        return Response(view('dashboard.reservation.create'));
     }
 
     public function doReservation(){
         // halaman untuk user melakukan reservasi
-        return Response(view());
+        return Response(view('dashboard.reservation.do-reservation'));
     }
     public function storeAddReservation(){
         // menambahkan data reservasi baru
